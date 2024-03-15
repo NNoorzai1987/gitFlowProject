ceb4120eb35de4415e66691c5c3ae36b32c28e60
Wellstudy1985 <Nnoorzai.001@gmail.com>
2024-03-15 00:40:19 -0700

    the new kba code added

---
+++ src/com/gitFlowkba/Main.java
@@ -0,0 +1,15 @@
+package com.gitFlowkba;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        kbaNewCode kbaNewCode= new kbaNewCode();
+        kbaNewCode.comID=465412;
+        kbaNewCode.appName="ocas";
+        kbaNewCode.size=1236.021;
+        System.out.println(kbaNewCode.appName);
+    }
+}
---
+++ src/com/gitFlowkba/kbaNewCode.java
@@ -0,0 +1,9 @@
+package com.gitFlowkba;
+
+public class kbaNewCode {
+    int comID= 13245654;
+    String appName="XIT";
+    Double size=1323.554;
+
+
+}
