3d80f493fb2564530be149d73d6e49ce5bb73e7e
Wellstudy1985 <Nnoorzai.001@gmail.com>
2024-03-15 01:05:04 -0700

    new code for kbaWT added

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        kbaWT kbaWT=new kbaWT();
+        System.out.println(kbaWT.size + ", " + kbaWT.word + ", " + kbaWT.str);
+
     }
 }
